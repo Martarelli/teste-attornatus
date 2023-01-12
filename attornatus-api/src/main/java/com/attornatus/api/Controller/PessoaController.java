e85546e3d45bbdde2f9d75c6fcf336b934aab916
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-01-12 19:00:43 -0300

    add dependencies and param Pessoa

--- attornatus-api/src/main/java/com/attornatus/api/Controller/PessoaController.java
+++ attornatus-api/src/main/java/com/attornatus/api/Controller/PessoaController.java
@@ -1,15 +1,19 @@
 package com.attornatus.api.Controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.attornatus.api.model.Pessoa;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaController {
 	
 	@GetMapping
-	public String hello() {
-		return "Olá Mundo";
+	public List<Pessoa> list() {
+		
 	}
 }
--- attornatus-api/src/main/java/com/attornatus/api/model/Pessoa.java
+++ attornatus-api/src/main/java/com/attornatus/api/model/Pessoa.java
@@ -2,4 +2,10 @@ package com.attornatus.api.model;
 
 public class Pessoa {
 
+	private String nome, dataNascimento, cidade, logradouro;
+	private int cep, numero;
+
+
+
+
 }
